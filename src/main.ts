fd31cad68d38a4bafc5a5ab5d618207068d40cf4
Ezequiel <ezequielw22l@gmail.com>
2025-11-12 14:20:43 -0300

    -- Se borro el boton de prueba junto a sus funciones

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,20 @@
-import { prueba } from "./utils/api";
 import { navigateTo, PATHS } from './utils/navigate';
 
-const button = document.getElementById("button");
-button?.addEventListener("click", () => {
-  prueba();
-});
+const userData = localStorage.getItem("user");
 
-// Si no estás en modo prueba, ejecuta la redirección
-const TEST_MODE = false; // ponelo en false cuando termines
-
-if (!TEST_MODE) {
-  const userData = localStorage.getItem("user");
-
-  if (userData) {
-    try {
-      const user = JSON.parse(userData);
-      if (user.role === "ADMIN") {
-        navigateTo(PATHS.ADMIN_HOME);
-      } else {
-        navigateTo(PATHS.STORE_HOME)
-      }
-    } catch (error) {
-      console.error("Error al leer los datos del usuario:", error);
-      localStorage.removeItem("user");
-      navigateTo(PATHS.LOGIN)
+if (userData) {
+  try {
+    const user = JSON.parse(userData);
+    if (user.role === "ADMIN") {
+      navigateTo(PATHS.ADMIN_HOME);
+    } else {
+      navigateTo(PATHS.STORE_HOME)
     }
-  } else {
+  } catch (error) {
+    console.error("Error al leer los datos del usuario:", error);
+    localStorage.removeItem("user");
     navigateTo(PATHS.LOGIN)
   }
+} else {
+  navigateTo(PATHS.LOGIN)
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,23 +5,6 @@ import type { IProduct } from '@/types/IProduct';
 import type { ICategoria } from '@/types/ICategoria';
 import type { IOrder, IOrderCreate, EstadoPedido } from '@/types/IOrders';
 
-
-//Prueba de conexion con el backend
-export async function prueba() {
-    const url = "http://localhost:8080/api/alive";
-    try {
-        const response = await fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' } });
-        console.log('response a');
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const result = await response.text();
-        console.log('resultado', result);
-    } catch (error: any) {
-        console.error(error.message);
-    }
-}
 /*
 ==============================================
     FUNCION GENERICA PARA HACER REQUESTS
